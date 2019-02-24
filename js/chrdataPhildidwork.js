3bc228f364e5e8377886c62a44ada78c315e0f2e
Victoria <vv0011@uah.edu>
2019-02-24 13:08:20 -0600

    before messing with chrdata for display

---
+++ js/chrdataPhildidwork.js
@@ -0,0 +1,40 @@
+/*
+var chrData =
+{
+  
+  
+  "M": {
+    "overlaps": [],
+    "ref_gaps": [],
+    "total": 16571,
+    "undefined": 0,
+    "nonoverlaps": []
+  },
+
+
+*/
+
+var  chrData=
+{   "Y": { 
+    "overlaps": [],
+    "total": 1000000,
+    "undefined": 36389037,
+"nonoverlaps":[  /* start */
+
+{  
+"start":  300000,   
+"sep":    10000 
+},
+{  "start":  350000, "gene": "NR_028057",  "sep":    10000 },
+{  "start":  400000,   "sep":    10000 },
+{  "start":  450000,   "sep":    10000 },
+{  "start":  500000,   "sep":    10000 },
+{  "start":  550000,   "sep":    10000 },
+{  "start":  600000,   "sep":    10000 },
+{  "start":  650000,   "sep":    10000 },
+{  "start":  850000, "gene": "NR_028075",  "sep":    10000 }
+
+
+] }, "name": "philtest"
+}   /*end */
+
